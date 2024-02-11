fd31761c6880ef6813b45b1b96956c9fea01ed92
rooddeeo <rooddeeo@gmail.com>
2024-02-11 15:46:44 +0200

    Update NodeJs Home Work 4

--- helpers/isValidId.js
+++ helpers/isValidId.js
@@ -3,8 +3,7 @@ import { HttpError } from "./HttpError.js";
 
 export const isValidId = (req, res, next) => {
 	const { id } = req.params;
-	const { owner } = req.user;
-	if (!isValidObjectId({ id, owner })) {
+	if (!isValidObjectId(id)) {
 		next(HttpError(400, `Sorry, is ID: ${id} not valid!`));
 	}
 	next();
